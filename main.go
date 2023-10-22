e1bda7f5777d42360261628120fbcf8575d81af1
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-10-22 14:05:25 +0700

    chore: implement upgraded dependency

--- main.go
+++ main.go
@@ -7,9 +7,19 @@ import (
 )
 
 func main() {
+	privateDomain := "dev,id"
+
 	testMail := randomail.GenerateRandomEmail()
 
 	emails := randomail.GenerateRandomEmails(10)
+
+	privateEmail, err := randomail.GenerateRandomEmailWithCustomDomain(privateDomain)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Println("Private Email:", privateEmail)
 
 	for _, email := range emails {
 		fmt.Println("Generate Email:", email)
